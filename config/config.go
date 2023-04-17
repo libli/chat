c62f56ee0994dc0ced59c1e25c7ef6509093ea93
libli <libli@tencent.com>
2023-04-17 10:49:37 +0800

    Revert "modify README.md"
    This reverts commit e85c850123464e094b183f7390559724caae2523.

--- config/config.go
+++ config/config.go
@@ -9,22 +9,10 @@ import (
 
 const configFilename = "config.yaml"
 
-// Config is the config struct.
 type Config struct {
-	OpenAIKey string      `yaml:"OpenAIKey"`
-	GinPort   string      `yaml:"GinPort"`
-	Driver    string      `yaml:"Driver"`
-	DBName    string      `yaml:"DBName"`
-	MySQL     MySQLConfig `yaml:"MySQL"`
-}
-
-// MySQLConfig is the MySQL config struct.
-type MySQLConfig struct {
-	Host     string `yaml:"Host"`
-	Port     string `yaml:"Port"`
-	Username string `yaml:"Username"`
-	Password string `yaml:"Password"`
-	DBName   string `yaml:"DBName"`
+	OpenAIKey string `yaml:"OpenAIKey"`
+	DBName    string `yaml:"DBName"`
+	GinPort   string `yaml:"GinPort"`
 }
 
 // Parse parses the config file and returns a Config struct.
@@ -39,10 +27,4 @@ func Parse() (*Config, error) {
 		return nil, fmt.Errorf("failed to parse config file: %w", err)
 	}
 	return c, nil
-}
-
-// MySQLDSN 生成 MySQL 的 DSN
-func (c *Config) MySQLDSN() string {
-	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		c.MySQL.Username, c.MySQL.Password, c.MySQL.Host, c.MySQL.Port, c.MySQL.DBName)
 }
